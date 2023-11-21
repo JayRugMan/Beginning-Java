a69847f4d8782a4b8105eec834b9f33ae7a7eaa4
jayrugman <jasonrhardman@gmail.com>
2023-11-20 23:52:12 -0700

    nested intro programs, created variable folder and programs

---
+++ variables/Mess.java
@@ -0,0 +1,19 @@
+public class Mess {
+	public static void main(String[] args) {   
+		int year = 2001;
+        String title = "Shrek";
+        char genre = 'C';
+        double runtime = 1.58;
+        boolean isPG = true;
+        System.out.print("Title: ");
+        System.out.println(title);
+        System.out.print("Year: ");
+        System.out.println(year);
+        System.out.print("Genre: ");
+        System.out.println(genre);
+        System.out.print("Runtime: ");
+        System.out.println(runtime);
+        System.out.print("Is it PG: ");
+        System.out.println(isPG);
+	}
+}
---
+++ variables/MyProfile.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+        String name = "Jason Hardman";
+        int age = 38;
+        double desiredSalary = 200000.00;
+        char gender = 'm';
+        boolean lookingForJob = false;
+    }
+}
