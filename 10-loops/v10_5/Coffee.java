3e649009f7652f698ed3cf469606a485a2586185
Jason Hardman <jason.hardman@churchofjesuschrist.org>
2024-01-09 17:47:44 -0700

    added module 10, loops

---
+++ 10-loops/v10_5/Coffee.java
@@ -0,0 +1,13 @@
+class Coffee {
+  
+  public static void main(String[] args) {
+    
+    for (int i = 1; i <= 100; i++) {
+      
+      System.out.println("Zoidberg drinks cup of coffee #" + i);
+      
+    }
+    
+  }
+  
+}
