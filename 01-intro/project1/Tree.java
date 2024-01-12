71cd0b81cce4dd22288d4d0f129a5183559d7cb4
Jason Hardman <jason.hardman@churchofjesuschrist.org>
2024-01-12 13:24:28 -0700

    added tree project

---
+++ 01-intro/project1/Tree.java
@@ -0,0 +1,16 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args) {
+    // Introduction printed to screen
+    System.out.println("Hi, I'm a husbandman. My name is Jason.");
+    // Image of tree printed to screen
+    System.out.println("Okay, let's plant this thing...\n");
+    System.out.println("       *");
+    System.out.println("      ***");
+    System.out.println("     *****");
+    System.out.println("    *******");
+    System.out.println("   *********"); 
+    System.out.println("  ***********");
+    System.out.println("       *");
+  }
+}
