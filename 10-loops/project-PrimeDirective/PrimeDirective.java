465787be02b1641feb617c518156e4af44b1b4ba
Jason Hardman <jason.hardman@churchofjesuschrist.org>
2024-01-31 12:19:48 -0700

    completed primedirective project

--- 10-loops/project-PrimeDirective/PrimeDirective.java
+++ 10-loops/project-PrimeDirective/PrimeDirective.java
@@ -16,18 +16,35 @@ public class PrimeDirective {
     return true;
   }
 
-  public static void main(String[] args) {
-  
-    PrimeDirective pd = new PrimeDirective();
-    //int[] numbers = {0, 1, 2, 6, 29, 28, 33, 11, 100, 101, 43, 89};
-    //for (int num: numbers) {
-    for(int num = 0; num <= 100; num++) {
-      if (pd.isPrime(num)) {
-        System.out.println(num + " is prime");
-      } else {
-        System.out.println(num + " is NOT prime");
+  public ArrayList<Integer> onlyPrimes(int[] numbers) {
+    ArrayList<Integer> primes = new ArrayList<Integer>();
+    for(int number: numbers) {
+      if(isPrime(number)) {
+        primes.add(number);
       }
     }
+    return primes;
+  }
+
+  public int[] setRange(int start, int end) {
+    int array_size = (end+1) - start;
+    int[] range = new int[array_size];
+    for(int i = 0; i < end; i++) {
+      range[i] = i + 1;
+    }
+    return range;
+  }
+
+  public static void main(String[] args) {
+    
+    int start = 0;
+    int end = 13;
+    PrimeDirective pd = new PrimeDirective();
+    int[] numbers = pd.setRange(start, end);
+    ArrayList<Integer> listOfPrimes = new ArrayList<Integer>(pd.onlyPrimes(numbers));
+    int totalPrimes = listOfPrimes.size();
+    System.out.println(listOfPrimes);
+    System.out.println("\nTotal number of primes between " + start + " and " + end + ": " + totalPrimes);
 
   }
 }
